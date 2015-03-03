d9e22e0a757b12998fc47d33e3bc721547583d6f
Christopher Lindor <chrislindor@gmail.com>
2015-03-02 19:10:53 -0500

    code simplified with functions
    code has been edited to add functions to make code more efficient

--- quiz_game.js
+++ quiz_game.js
@@ -1,88 +1,41 @@
 /*
-Quiz game using conditional statements,
-and boolean principles
+Quiz game V2 coded with functions instead of repeating variables
 */
 
 // Establishing initial variable values;
 var numberCorrect = 0;
 
-//Asking question and checking to see if answer correct;
-var playerAnswer1 = prompt('What is the parent company of Instagram?');
-if (playerAnswer1.toLowerCase() === 'facebook') {
-    numberCorrect += 1;
-    alert('Congrats you have answered ' + numberCorrect + ' correctly');
-} else {
-    alert('Im sorry but that was wrong, better luck on the next question');
+//Establishing Function for asking the question and outputting score;
+function questionRoot(questionContent, questionAnswer) {
+    var playerResponse = prompt(questionContent);
+    if (playerResponse.toLowerCase() === questionAnswer.toLowerCase()) {
+        numberCorrect += 1;
+        alert('Correct!!! you have answered ' + numberCorrect + ' questions so far.');
+    } else {
+        alert('You got that one wrong the correct answer is ' + questionAnswer);
+    }
+    return numberCorrect;
+    
 }
 
-/*
-Second question checks to see if they have gotten any correct yet if not
-offers some encourgement
-*/
-var playerAnswer2 = prompt('What company makes the iPad?');
-if (playerAnswer2.toLowerCase() === 'apple') {
-    numberCorrect += 1;
-    alert('Congrats you have answered ' + numberCorrect + ' correctly');
-} else if (numberCorrect === 0) {
-    alert('Come on you havent gotten any questions right yet');
-} else {
-    alert('You have gotten ' + numberCorrect + ' correct so far');
-}
+//inputing arguments and executing  function questionRoot;
+var question1 = questionRoot('Who is the parent company of Instagram', 'facebook');
 
-/* Third question with checks to see how many 
-have been anwsered correctly and displays message based 
-on number correct
-*/
-var playerAnswer3 = prompt('What company makes the surface tablet');
-if (playerAnswer3.toLowerCase() === 'microsoft') {
-    numberCorrect += 1;
-    alert('Congrats you have answered ' + numberCorrect + ' correctly');
-} else if (numberCorrect === 0) {
-    alert('Are you that stupid this isn\'t hard');
-} else if (numberCorrect === 1) {
-    alert('Only ' + numberCorrect + '  you can do better');
-} else if (numberCorrect === 2) {
-    alert('You got ' + numberCorrect + ' so far not bad');
-}
+var question2 = questionRoot('What company makes the iPad?', 'apple');
 
-// another question with multiple answer outcomes;
-var playerAnswer4 = prompt('What team does Kobe Bryant play for');
-if (playerAnswer4.toLowerCase() === 'lakers' || playerAnswer4.toLowerCase === 'the lakers') {
-    numberCorrect += 1;
-    alert('Congrats you have answered ' + numberCorrect + ' correctly');
-} else if (numberCorrect === 0) {
-    alert('You are a special kind of stupid to have ' + numberCorrect + ' correct');
-} else if (numberCorrect === 1) {
-    alert('only ' + numberCorrect + ' LOL come on dont make me laugh');
-} else if (numberCorrect === 2) {
-    alert('You got ' + numberCorrect + ' try harder');
-} else if (numberCorrect === 3) {
-    alert('You got ' + numberCorrect + ' so far not bad keep it up');
-}
+var question3 = questionRoot('Which company makes the Surface Pro Tablet', 'microsoft');
 
-//Final question;
-var playerAnswer5 = prompt('Who is the founder of microsoft?');
-if (playerAnswer5.toLowerCase() === 'bill gates') {
-    numberCorrect += 1;
-    alert('Congrats you have answered ' + numberCorrect + ' correctly');
-} else if (numberCorrect === 0) {
-    alert(' I feel sorry that you are the dumbest person on the internet. ' + numberCorrect + ' correct.');
-} else if (numberCorrect === 1) {
-    alert('you are kind of pitiful to have ' + numberCorrect + ' correct');
-} else if (numberCorrect === 2) {
-    alert('You got ' + numberCorrect + ' not that bad could be better');
-} else if (numberCorrect === 3) {
-    alert('You got ' + numberCorrect + ' barely passed ');
-} else if (numberCorrect === 4) {
-    alert('You got ' + numberCorrect + ' alsmost perfect');
-}
+var question4 = questionRoot('What team does Kobe Bryant play for', 'lakers');
+
+var question5 = questionRoot('Who is the founder of microsoft?', 'bill gates');
+
 
 //Begin ranking and badge deffinition;
 if (numberCorrect === 5) {
     document.write('<p>You got the GOld medal for answering ' + numberCorrect + ' correct amazing job</p>');
-} else if (numberCorrect <= 4 && numberCorrect >= 3) {
+} else if (numberCorrect >= 3) {
     document.write('<p>you got the SLIVER medal for answering ' + numberCorrect + ' correct good job</p>');
-} else if (numberCorrect <= 2 && numberCorrect >= 1) {
+} else if (numberCorrect >= 1) {
     document.write('<p> you got the bronze medal for answering ' + numberCorrect + ' correct you are average');
 } else if (numberCorrect === 0) {
     document.write('You need to reboot your brain for being a complete DUMB ASS!!!!');
