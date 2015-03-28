605a975c06cfefd86f22302531f42a6db6ec3c9d
Christopher Lindor <chrislindor@gmail.com>
2015-03-28 16:04:48 -0400

    refactored
    Refactored code to remove unnecessary variables and lines of code
    within the function.

--- quiz_game.js
+++ quiz_game.js
@@ -10,24 +10,23 @@ function questionRoot(questionContent, questionAnswer) {
     var playerResponse = prompt(questionContent);
     if (playerResponse.toLowerCase() === questionAnswer.toLowerCase()) {
         numberCorrect += 1;
-        alert('Correct!!! you have answered ' + numberCorrect + ' questions so far.');
+        alert('Correct!!! you have answered ' + numberCorrect + ' questions correct so far.');
     } else {
         alert('You got that one wrong the correct answer is ' + questionAnswer);
     }
-    return numberCorrect;
     
 }
 
 //inputing arguments and executing  function questionRoot;
-var question1 = questionRoot('Who is the parent company of Instagram', 'facebook');
+questionRoot('Who is the parent company of Instagram', 'facebook');
 
-var question2 = questionRoot('What company makes the iPad?', 'apple');
+questionRoot('What company makes the iPad?', 'apple');
 
-var question3 = questionRoot('Which company makes the Surface Pro Tablet', 'microsoft');
+questionRoot('Which company makes the Surface Pro Tablet', 'microsoft');
 
-var question4 = questionRoot('What team does Kobe Bryant play for', 'lakers');
+questionRoot('What team does Kobe Bryant play for', 'lakers');
 
-var question5 = questionRoot('Who is the founder of microsoft?', 'bill gates');
+questionRoot('Who is the founder of microsoft?', 'bill gates');
 
 
 //Begin ranking and badge deffinition;
@@ -38,7 +37,7 @@ if (numberCorrect === 5) {
 } else if (numberCorrect >= 1) {
     document.write('<p> you got the bronze medal for answering ' + numberCorrect + ' correct you are average');
 } else if (numberCorrect === 0) {
-    document.write('You need to reboot your brain for being a complete DUMB ASS!!!!');
+    document.write('You need to reboot your brain !!!!');
 }
 var finalMessage = ('<a href="index.html"><h4>Click here to play again</h4></a>');
 document.write(finalMessage);
